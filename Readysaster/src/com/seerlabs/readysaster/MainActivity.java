4b8d5a0146a0f1fe29a74dd9c14a53ae1c9922c4
Gian Samson <gian.samson@seer-technologies.com>
2014-05-10 22:57:12 +0800

    home page

--- Readysaster/src/com/seerlabs/readysaster/MainActivity.java
+++ Readysaster/src/com/seerlabs/readysaster/MainActivity.java
@@ -1,15 +1,11 @@
 package com.seerlabs.readysaster;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+
+import com.seerlabs.readysaster.fragments.HomeFragment;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -20,7 +16,7 @@ public class MainActivity extends ActionBarActivity {
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+					.add(R.id.container, new HomeFragment()).commit();
 		}
 	}
 
@@ -43,22 +39,4 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
-
 }
---
+++ Readysaster/src/com/seerlabs/readysaster/fragments/HomeFragment.java
@@ -0,0 +1,20 @@
+package com.seerlabs.readysaster.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.seerlabs.readysaster.R;
+
+public class HomeFragment extends Fragment {
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_main, container,
+				false);
+		return rootView;
+	}
+	
+}
